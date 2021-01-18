e176c7a41914c38ab63918895ee38d67bbd69503
wangxinyu123 <1348651096@qq.com>
2021-01-18 14:27:36 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TodoList from './TodoList'
+import { Provider } from 'react-redux'
+import store from './store'
+
+const App = (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  App,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
